44581476f3b15e1961d38899c95e3bbbf20f2a5a
Lea Oertlin <lea.oertlin@student.uibk.ac.at>
2022-04-06 16:00:09 +0200

    main.js

---
+++ codecademy/js_variables/main.js
@@ -0,0 +1,56 @@
+var favoriteFood = "pizza"
+var numOfSlices = 8
+console.log(favoriteFood)
+console.log(numOfSlices)
+
+let changeMe = true
+changeMe = false
+console.log(changeMe)
+
+const entree = "Enchiladas"
+console.log(entree)
+entree="Tacos"
+
+let levelUp = 10;
+let powerLevel = 9001;
+let multiplyMe = 32;
+let quarterMe = 1152;
+
+// Use the mathematical assignments in the space below:
+levelUp += 5
+console.log(levelUp)
+powerLevel -= 100
+console.log(powerLevel)
+multiplyMe *= 11
+console.log(multiplyMe)
+quarterMe /= 4
+console.log(quarterMe)
+
+// These console.log() statements below will help you check the values of the variables.
+// You do not need to edit these statements. 
+console.log('The value of levelUp:', levelUp); 
+console.log('The value of powerLevel:', powerLevel); 
+console.log('The value of multiplyMe:', multiplyMe); 
+console.log('The value of quarterMe:', quarterMe);
+
+let gainedDollar = 3;
+let lostDollar = 50;
+
+gainedDollar++;
+console.log(gainedDollar);
+
+lostDollar--;
+console.log(lostDollar)
+
+var favoriteAnimal = "dolphine"
+console.log("My favorite animal: " + favoriteAnimal)
+
+var myName = "Lea"
+var myCity = "Riederalp"
+console.log (`My name is ${myName}. My favorite city is ${myCity}.`)
+
+let newVariable = 'Playing around with typeof.';
+console.log(typeof newVariable)
+newVariable = 1
+console.log(typeof newVariable)
+
