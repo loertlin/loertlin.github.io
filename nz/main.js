4b6660e5b3774ca1f188cbd025d3efa1ce455762
Lea Oertlin <lea.oertlin@student.uibk.ac.at>
2022-04-07 15:44:54 +0200

    lat, lng, zoom variablen eingeführt

--- nz/main.js
+++ nz/main.js
@@ -1,13 +1,16 @@
 /*Neuseelandreise Skript*/
 //einzeiliger Kommentar
 
-
-var map = L.map('map').setView([-45.34, 170.82], 8);
+let lat = -45.34
+let lng = 170.82
+let zoom= 8
+let map = L.map('map').setView([lat, lng], zoom);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
-L.marker([-45.34, 170.82]).addTo(map)
+L.marker([lat, lng]).addTo(map)
     .bindPopup('<h3>Moeraki Boulders</h3>')
     .openPopup();
+
